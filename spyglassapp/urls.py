578d69e1b16a51c0fe5a229bef7479f5cd9d0e25
will-gerard <whg8hc@virginia.edu>
2016-12-31 14:28:52 -0500

    display list of nyc venues

--- spyglassapp/urls.py
+++ spyglassapp/urls.py
@@ -5,4 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^select_city/$', views.select_city, name='select_city'),
+	url(r'^venue_list/$', views.new_york_city_venues, name='new_york_city_venues'),
 ]
--- spyglassapp/views.py
+++ spyglassapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+import urllib.request
+import urllib.parse
+import urllib
+from django.http import HttpResponse
+from django.conf import settings
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'spyglassapp/index.html')
+
+def select_city(request):
+    return render(request, 'spyglassapp/select_city.html')
+
+def new_york_city_venues(request):
+    req = urllib.request.Request("https://api.tripexpert.com/v1/venues?destination_id=6&api_key=" + settings.TRIPEXPERT_API_KEY, headers={'User-Agent': 'Mozilla/5.0'})
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    context = {'venues': resp['response']['venues']}
+    return render(request, 'spyglassapp/venue_list.html', context)
